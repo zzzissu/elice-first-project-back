40c8936f2bebf8c636daafdbb0f4e0af76dc9ae4
[Node1] 이수엽 <lsytown@naver.com>
2024-10-08 06:22:46 +0000

    Revert "Merge branch 'feat/user' into 'master'"
    This reverts merge request !1

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from "express";
-import { errorHandler } from "./src/middleware/error-handler.middleware.js";
-import { db_connect } from "./src/db/db.js";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
+import { dbConnect } from "./db/db.js";
 import cors from "cors";
-// import { user_router } from "./src/routes/user.routes.js";
+import { user_router } from "./routes/user_router.js";
 
 const app = express();
-const connection = await db_connect();
+await dbConnect();
 
-app.use(cors({ origin: "*" }),);
+app.arguments(cors({ origin: "*" }),);
 
 // Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.urlencoded({ extended: true }));
 // Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.json());
 
-// app.use('/users', user_router);
+app.use('/users', user_router);
 
 app.use(errorHandler);
 app.listen(3000, () => {
