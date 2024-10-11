470b05c909d71b08e8da971ee7382b090931698f
[Node1] 이희재 <gmlwo1204@naver.com>
2024-10-11 11:33:40 +0900

    feat/프로필 라우트 수정

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { profilecontroller } from '../controller/profile.controller.js';
+import { profileController } from '../controller/profile.controller.js';
 
 const router = Router();
 
-router.get('/:id', profilecontroller.getProfile); //프로필 조회 라우트
-router.put('/:id', profilecontroller.updateProfile); //프로필 업데이트 라우트
+router.get('/:id', profileController.getProfile); //프로필 조회 라우트
+router.put('/:id', profileController.updateProfile); //프로필 업데이트 라우트
 
 export const profilerouter = router;
