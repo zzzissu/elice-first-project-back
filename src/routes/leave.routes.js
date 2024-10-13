936f658e896ba54a1c87de4f6f5daed290c1431b
[Node1] 이희재 <gmlwo1204@naver.com>
2024-10-13 16:10:18 +0900

    feat/연차 관련 컨트롤러 수정, 주석 추가

--- src/routes/leave.routes.js
+++ src/routes/leave.routes.js
@@ -3,8 +3,8 @@ import { leaveController } from '../controller/leave.controller.js';
 
 const router = Router();
 
-router.put('/:id/useleave', leaveController.useLeave);
+router.put('/:id/useleave', leaveController.useLeave);  //연차 사용 라우터
 
-router.get('/:id', leaveController.getUserLeave);
+router.get('/:id', leaveController.getUserLeave);  //연차 정보 조회 라우터
 
 export const leaverouter = router;
--- src/service/leave.service.js
+++ src/service/leave.service.js
@@ -1,11 +1,11 @@
 import { leaveModel } from '../models/leave.model.js';
 
 export const leaveService = {
-    
+    //남은 연차 정보 가져옴
     getUserLeave: async (userId) => {
         return await leaveModel.getUserLeave(userId);
     },
-
+    //남은 연차 업데이트
     updateUserLeave: async (userId, remainingLeave) => {
         return await leaveModel.updateUserLeave(userId, remainingLeave);
     }
