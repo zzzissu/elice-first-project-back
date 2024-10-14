f269ac8881d41d76a348f9ded26e8c66359fa7d9
[Node1] 이수엽 <lsytown@naver.com>
2024-10-14 12:24:32 +0900

    refactor/delete api router

--- src/routes/announcement.routes.js
+++ src/routes/announcement.routes.js
@@ -6,6 +6,6 @@ const router = Router();
 
 router.get('/api', announcementController.getAllAnnounce);
 router.post('/api/post', authMiddleware.verifyToken, announcementController.postAnnounce);
-router.delete('/api/announcement/:schedule_id', authMiddleware.verifyToken, announcementController.deleteAnnounce);
+router.delete('/api/:schedule_id', authMiddleware.verifyToken, announcementController.deleteAnnounce);
 
 export const announcementRouter = router;
