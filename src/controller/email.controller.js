2e09cb1d8cae5b38b8affa933413bd524f0d3ba7
[Node1] 이수엽 <lsytown@naver.com>
2024-10-15 15:22:05 +0900

    feat/email - 삭제기능, 알림기능

--- src/controller/email.controller.js
+++ src/controller/email.controller.js
@@ -25,6 +25,35 @@ export const emailController = {
     }
   },
 
+  // 새로운 이메일 확인 (알림)
+  checkNewEmail: async (req, res, next) => {
+    try {
+      const userEmail = req.user.email;
+      const newEmailCount = await emailService.checkNewEmail(userEmail);
+
+      res.status(200).json({
+        newEmailCount, // 새로운 이메일 개수 반환
+        message: newEmailCount > 0 ? '새로운 이메일이 있습니다.' : '새로운 이메일이 없습니다.'
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  // 이메일 읽음 처리
+  checkedEmail: async (req, res, next) => {
+    try {
+      const { email_id } = req.params;
+      const userEmail = req.user.email;
+      const email = await emailService.checkedEmail(email_id, userEmail);
+
+      res.status(200).json(email);
+    } catch(e) {
+      next(e);
+    }
+  },
+
+
   // 이메일 작성
   postEmail: async (req, res, next) => {
     try {
@@ -44,4 +73,17 @@ export const emailController = {
     }
   },
 
+  // 이메일 삭제
+  deleteEmail: async (req, res, next) => {
+    try {
+      const { email_id } = req.params;
+      const userEmail = req.user.email;
+
+      const result = await emailService.deleteEmail(email_id, userEmail);
+
+      res.status(200).json(result);
+    } catch(e) {
+      next(e);
+    }
+  },
 }
--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -6,6 +6,10 @@ const router = Router();
 
 router.get('/api/received', authMiddleware.verifyToken, emailController.getReceivedEmail);
 router.get('/api/sent', authMiddleware.verifyToken, emailController.getSentEmail);
+router.patch('/api/check/:email_id', authMiddleware.verifyToken, emailController.checkedEmail)
 router.post('/api/post', authMiddleware.verifyToken, emailController.postEmail);
+router.delete('/api/:email_id', authMiddleware.verifyToken, emailController.deleteEmail);
+
+router.get('/api/check', authMiddleware.verifyToken, emailController.checkNewEmail);
 
 export const emailRouter = router;
