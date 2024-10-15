e3040b27d96b83a77e9792dc5786bd8b473e49e7
[Node1] 이수엽 <lsytown@naver.com>
2024-10-16 00:13:39 +0900

    refactor/라우터 변경, 포트변경

--- src/routes/approval.routes.js
+++ src/routes/approval.routes.js
@@ -5,11 +5,11 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 const router = Router();
 
 // API
-router.post('/api/annual', authMiddleware.verifyToken, approvalController.postAnnual); // 연차 신청서
-router.post('/api/outside', authMiddleware.verifyToken, approvalController.postOutside); // 외근 신청서
-router.post('/api/businessreport', authMiddleware.verifyToken, approvalController.postBusinessReport); // 업무 보고서
+router.post('/annual', authMiddleware.verifyToken, approvalController.postAnnual); // 연차 신청서
+router.post('/outside', authMiddleware.verifyToken, approvalController.postOutside); // 외근 신청서
+router.post('/businessreport', authMiddleware.verifyToken, approvalController.postBusinessReport); // 업무 보고서
 
-router.get('/api/wait',approvalController.getAllWaitApproval); // 결재 대기중
-router.get('/api/confirm',approvalController.getAllConfirmedApproval); // 결재 완료
+router.get('/wait',approvalController.getAllWaitApproval); // 결재 대기중
+router.get('/confirm',approvalController.getAllConfirmedApproval); // 결재 완료
 
 export const approvalRouter = router;
