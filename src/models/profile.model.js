2a3446fe0c8a9de5a5ac558db31067583df1ec0e
[Node1] 이희재 <gmlwo1204@naver.com>
2024-10-15 22:26:41 +0900

    feat/프로필 모델 수정

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -1,36 +1,25 @@
-import { dbConnect }  from '../db/db.js';
-
-
+import { dbConnect} from '../db/db.js';
 
 export const profileModel = {
-  getUserProfile: async (userId) => {
-    const db = await dbConnect();
-    const query = 'SELECT * FROM user WHERE id = ?';
-    const [rows] = await db.execute(query, [userId]);  // db 객체에서 execute() 호출
-    return rows[0];
-  },
+    // 사용자 프로필 조회 
+    getUserProfile: async (userId) => {
+        const db = await dbConnect();
+        const query = 'SELECT * FROM user WHERE id = ?';
+        const [rows] = await db.execute(query, [userId]);
+        return rows[0];
+    },
 
-  // 프로필 수정
-  updateUserProfile: async (userId, { phone, profileImage }) => {
-    const db = await dbConnect();
-    let query = 'UPDATE user SET ';
-    const queryParams = [];
+    // 전화번호 업데이트 
+    updatePhoneNumber: async (userId, phone) => {
+        const db = await dbConnect();
+        const query = 'UPDATE user SET phone = ? WHERE id = ?';
+        await db.execute(query, [phone, userId]);
+    },
 
-    if (phone) {
-      query += 'phone = ?, ';
-      queryParams.push(phone);
+    // 프로필 사진 업데이트 
+    updateProfileImage: async (userId, profileImage) => {
+        const db = await dbConnect();
+        const query = 'UPDATE user SET profile_image = ? WHERE id = ?';
+        await db.execute(query, [profileImage, userId]);
     }
-
-    if (profileImage) {
-      query += 'profile_image = ?, ';
-      queryParams.push(profileImage);
-    }
-
-    query = query.slice(0, -2); // 마지막 쉼표 제거
-    query += ' WHERE id = ?';
-    queryParams.push(userId);
-
-    const [result] = await db.execute(query, queryParams);  // db 객체에서 execute() 호출
-    return result;
-  },
 };
