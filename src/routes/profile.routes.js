6983e92ebfaeac0f9863cb90b1f99e22aa008ef5
[Node1] 이희재 <gmlwo1204@naver.com>
2024-10-15 22:27:17 +0900

    feat/프로필 라우트 수정

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-import { profileController } from '../controller/profile.controller.js';
+import { updatePhoneNumber, updateProfileImage, getProfile } from '../controller/profile.controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
+import { upload } from '../middleware/upload.middleware.js';  
 
 const router = Router();
 
-router.get('/', authMiddleware.verifyToken, profileController.getProfile); //유저 정보 가져오기
-router.put('/', authMiddleware.verifyToken, profileController.updateProfile); //유저 정보 업데이트
+// 프로필 조회
+router.get('/', authMiddleware.verifyToken, getProfile);
+
+// 전화번호 업데이트
+router.put('/phone', authMiddleware.verifyToken, updatePhoneNumber);
+
+// 프로필 사진 업데이트
+router.put('/image', authMiddleware.verifyToken, upload.single('profileImage'), updateProfileImage);
 
 export const profilerouter = router;
