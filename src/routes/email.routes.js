ec8bd6931ecb3a67de7ae880b1e37fa30d9d2a25
[Node1] 이수엽 <lsytown@naver.com>
2024-10-15 18:15:55 +0900

    refactor/emailRoutes

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -4,12 +4,12 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get('/api/received', authMiddleware.verifyToken, emailController.getReceivedEmail);
-router.get('/api/sent', authMiddleware.verifyToken, emailController.getSentEmail);
-router.patch('/api/check/:email_id', authMiddleware.verifyToken, emailController.checkedEmail)
-router.post('/api/post', authMiddleware.verifyToken, emailController.postEmail);
-router.delete('/api/:email_id', authMiddleware.verifyToken, emailController.deleteEmail);
+router.get('/received', authMiddleware.verifyToken, emailController.getReceivedEmail);
+router.get('/sent', authMiddleware.verifyToken, emailController.getSentEmail);
+router.patch('/check/:email_id', authMiddleware.verifyToken, emailController.checkedEmail)
+router.post('/post', authMiddleware.verifyToken, emailController.postEmail);
+router.delete('/:email_id', authMiddleware.verifyToken, emailController.deleteEmail);
 
-router.get('/api/check', authMiddleware.verifyToken, emailController.checkNewEmail);
+router.get('/check', authMiddleware.verifyToken, emailController.checkNewEmail);
 
 export const emailRouter = router;
