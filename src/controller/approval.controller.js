ba88f28acee59fe98f0f8cc39e090c50cda27dbc
[Node1] 이수엽 <lsytown@naver.com>
2024-10-17 20:29:11 +0900

    feat/결재 현황 카운트

--- src/controller/approval.controller.js
+++ src/controller/approval.controller.js
@@ -1,6 +1,19 @@
 import { approvalService } from "../service/approval.service.js";
 
 export const approvalController = {
+  // 결재 현황 조회
+  getApproval: async (req, res, next) => {
+    try {
+      const userName = req.user.name;
+      if (!userName) throw new Error ('Unauthorized+유저 정보를 찾을 수 없음');
+
+      const result = await approvalService.getApproval(userName);
+      res.status(200).json(result);
+    } catch(e) {
+      next(e);
+    }
+  },
+  
   // 연차 신청서
   postAnnual: async (req, res, next) => {
     try {
@@ -9,7 +22,7 @@ export const approvalController = {
       const user_id = req.user.id;
       const annual_leave = req.user.annual_leave;
 
-      if (!user_id) throw new Error ('Bad Request+유저 정보를 찾을 수 없음');
+      if (!user_id) throw new Error ('Unauthorized+유저 정보를 찾을 수 없음');
 
       // 날짜 차이를 구하기 위한 계산
       const startDate = new Date(start_date);
@@ -36,7 +49,7 @@ export const approvalController = {
       const user_name = req.user.name;
       const user_id = req.user.id;
 
-      if (!user_id) throw new Error ('Bad Request+유저 정보를 찾을 수 없음');
+      if (!user_id) throw new Error ('Unauthorized+유저 정보를 찾을 수 없음');
 
       const result = await approvalService
       .postOutside({ start_date, finish_date, content }, user_name, user_id);
@@ -53,7 +66,7 @@ export const approvalController = {
       const user_name = req.user.name;
       const user_id = req.user.id;
 
-      if (!user_id) throw new Error ('Bad Request+유저 정보를 찾을 수 없음');
+      if (!user_id) throw new Error ('Unauthorized+유저 정보를 찾을 수 없음');
 
       const result = await approvalService
       .postBusinessReport({ title, start_date, finish_date, content, request, significant }, user_name, user_id);
--- src/routes/approval.routes.js
+++ src/routes/approval.routes.js
@@ -9,6 +9,7 @@ router.post('/annual', authMiddleware.verifyToken, approvalController.postAnnual
 router.post('/outside', authMiddleware.verifyToken, approvalController.postOutside); // 외근 신청서
 router.post('/businessreport', authMiddleware.verifyToken, approvalController.postBusinessReport); // 업무 보고서
 
+router.get('/count', authMiddleware.verifyToken, approvalController.getApproval); // 결재 현황 조회
 router.get('/wait',approvalController.getAllWaitApproval); // 결재 대기중
 router.get('/confirm',approvalController.getAllConfirmedApproval); // 결재 완료
 
